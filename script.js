ab6bdb6d064845d06739ef6dac7d67353cd2b680
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-03-10 17:10:50 +0100

    allow users to change grid size

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const container = document.querySelector("#container");
+const containerSize = 480;
+container.style.height = `${containerSize}px`;
+container.style.width = `${containerSize}px`;
 
-const size = 16;
+const defaultGridSize = 16;
 
-for (let i = 0; i < size * size; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "red";
-  });
-  container.appendChild(square);
+function createGrid(gridSize) {
+  const squareSize = containerSize / gridSize;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.style.cssText = `width: ${squareSize}px; height: ${squareSize}px`;
+    square.classList.add("square");
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "red";
+    });
+    container.appendChild(square);
+  }
 }
+
+createGrid(defaultGridSize);
+
+const sizeBtn = document.querySelector("#size-btn");
+
+sizeBtn.addEventListener("click", () => {
+  const newSize = prompt("Enter new size for the grid (1-100):");
+  if (newSize < 1 || newSize > 100) return;
+  container.innerHTML = "";
+  createGrid(newSize);
+});
