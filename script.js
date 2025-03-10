04565cff3d1dd730a4bb921a48adae5e42ce5e7d
Rodrigo <200969810+Rod-97@users.noreply.github.com>
2025-03-10 03:16:50 +0100

    hover effect to change color

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ const size = 16;
 for (let i = 0; i < size * size; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "red";
+  });
   container.appendChild(square);
 }
